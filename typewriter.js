9e842a25b0b898c4d103f121168aa8ef4cd8cb36
Przemurb <przemurb@hotmail.it>
2022-10-28 14:24:52 +0200

    poprawiono bug dodawania znaku spacji

--- typewriter.js
+++ typewriter.js
@@ -1,13 +1,17 @@
 function addCharToText(char) {
     let word = document.getElementById("greating");
-    word.innerText += char;
+    word.innerHTML += char;
 }
 
 function typewriter(string, lentancy) {
-    let chars = string.split("");
-    for (let i = 0; i < chars.length; i++) {
-        setTimeout(() => addCharToText(chars[i]), (i * lentancy))
+//     let chars = string.split("");
+//     for (let i = 0; i < chars.length; i++) {
+//         setTimeout(() => addCharToText(chars[i]), (i * lentancy))
+//     }
+    for(let i = 0; i < string.length; i++) {
+        setTimeout(() => addCharToText(string.charAt(i)), (i * lentancy));
     }
+
 }
 
 typewriter("Hello my friend!", 200);
